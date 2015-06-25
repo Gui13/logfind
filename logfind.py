bb429cea7be0af051e6e4cb6ff656730b22c1303
Guillaume BIENKOWSKI <guitreize@gmail.com>
2015-06-25 22:21:48 +0200

    Functional version. needs testing now

--- logfind.py
+++ logfind.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 import os
-import sys
 import argparse
 import glob
 
@@ -12,24 +11,38 @@ pref_file = '~/.logfind'
 parser = argparse.ArgumentParser(prog='logfind', description='Finds things in logs. You should specify the list of files to consider in the ~/.logfind file (regexps allowed).')
 parser.add_argument('patterns', metavar='PATTERN', type=str, nargs='+',
                     help='''The list of words to search for. All words should be found for the logfile to match.''')
-parser.add_argument('-o', dest='or', type=bool,
+parser.add_argument('-o', dest='or_operator', type=bool,
                     help='''Will change the matcher to match at least one of the words instead of all.''')
 
 
+# todo: sinplify the loop maybe?
+def find_words_in_file(filename, words, match_one):
+    with open(filename) as logfile:
+        found_words = 0
+        for line in logfile:
+            for word in words:
+                if word in line:
+                    found_words += 1
+                    if match_one:
+                        break
+        if found_words == len(words) or (found_words != 0 and match_one):
+            print('{filename}'.format(filename=filename))
+
 if __name__ == '__main__':
-    parser.parse_args()
+    args = parser.parse_args()
 
     files = []
 
     try:
+        # read prefs file and build list of files
         prefs = open(os.path.expanduser(pref_file), 'r')
         for line in prefs:
             line = os.path.expanduser(line)
             line = os.path.expandvars(line)
             files += [f for f in glob.glob(line) if os.path.isfile(f)]
-        print('We need to check these files: {dirs}'.format(dirs=files))
 
-
+        for f in files:
+            find_words_in_file(f, args.patterns, args.or_operator)
 
     except FileNotFoundError as e:
         print('The file {pref} was not found. logfind will not search anywhere. Please create this file and specify the list of files to search in it.'.format(pref=pref_file))
